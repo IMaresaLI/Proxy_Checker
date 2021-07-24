a15b875099ca8f9c7f229b549680dcd08177b473
Batuhan Olgaç <70446049+IMaresaLI@users.noreply.github.com>
2021-07-25 00:22:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="proxychecker_Mp",
-    version="0.0.1",
+    version="0.0.2",
     author="Batuhan Olgac",
     author_email="mares4l@hotmail.com",
     description="You can check the availability of the proxy.",
