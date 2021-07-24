36cfc35fec8197e44fe4383e30e577fc165288c9
Batuhan Olgaç <70446049+IMaresaLI@users.noreply.github.com>
2021-07-24 23:09:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="proxychecker-Mp",
+    name="proxychecker_Mp",
     version="0.0.1",
     author="Batuhan Olgac",
     author_email="mares4l@hotmail.com",
